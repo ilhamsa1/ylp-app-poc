a69d183f5288bafb69cb3c6543737bdd18985761
ilhamsa1 <ilhamstomdevva@gmail.com>
2020-03-10 17:15:18 +0700

    update mainUrl

--- config.js
+++ config.js
@@ -1,12 +1,29 @@
 const dotenv = require('dotenv')
-const config = {
-  accountSid: process.env.TWILIO_ACCOUNT_SID,
-  authToken: process.env.TWILIO_AUTH_TOKEN,
-  twimlAppSid: process.env.TWILIO_TWIML_APP_SID,
-  callerId: process.env.TWILIO_CALLER_ID,
-  pushCredentialId: process.env.TWILIO_PUSH_CREDINTIAL_SID,
-  mainUrl: process.env.MAIN_URL,
-  port: process.env.PORT || 5009,
+const cfg = {};
+
+if (process.env.NODE_ENV !== 'test') {
+  dotenv.config({path: '.env'});
+} else {
+  dotenv.config({path: '.env.example', silent: true});
 }
 
-module.exports = config
+// HTTP Port to run our web application
+cfg.port = process.env.PORT || 3000
+
+// Your Twilio account SID and auth token, both found at:
+// https://www.twilio.com/user/account
+//
+// A good practice is to store these string values as system environment
+// variables, and load them from there as we are doing below. Alternately,
+// you could hard code these values here as strings.
+cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
+cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
+
+cfg.twimlAppSid = process.env.TWILIO_TWIML_APP_SID;
+cfg.callerId = process.env.TWILIO_CALLER_ID;
+cfg.pushCredentialId = process.env.TWILIO_PUSH_CREDINTIAL_SID;
+cfg.pushCredentialId = process.env.TWILIO_PUSH_CREDINTIAL_SID;
+
+cfg.mainUrl = process.env.MAIN_URL
+// Export configuration object
+module.exports = cfg;
